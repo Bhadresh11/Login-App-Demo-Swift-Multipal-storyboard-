cf211b9555a8190a60fe5b52cfe061f915b4ebdb
Bhadresh <bhadresh.patoliya@codewizinc.com>
2017-01-06 15:53:51 +0530

    simple Login

---
+++ Login App Demo Swift/Login App Demo Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Login App Demo Swift
+//
+//  Created by pc on 1/6/17.
+//  Copyright © 2017 Bhadresh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
